5cd5c7690b921bfab8aa8c01a9a6b110e8b5fc6a
Esraaelsayed <sosoelsayed14@gmail.com>
2025-10-27 02:30:28 +0300

    Update container class for ProjectsPage layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -32,10 +32,10 @@ const projects = [
 
 const ProjectsPage = () => {
   return (
-    <div className="container mt-20 py-10" id="contact">
+    <div className="container md:h-screen mt-20 py-10">
       <h1 className="text-5xl font-bold text-center mb-10">Our Projects</h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 ">
         {projects.map((project) => (
           <Link
             href={`/projects/${project.id}`}
